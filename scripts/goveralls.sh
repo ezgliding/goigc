ef659858f93de7767738689b8c181e9e2422886d
Ricardo Rocha <rocha.porto@gmail.com>
2017-08-07 23:23:30 +0200

    filter out vendor dir on go test/vet

--- scripts/goveralls.sh
+++ scripts/goveralls.sh
@@ -3,10 +3,10 @@ rc=0
 export PATH=$PATH:$HOME/gopath/bin
 golint .
 if [ $? -ne 0 ]; then rc=1; fi
-go vet ./...
+go list ./... | grep -v vendor | xargs go vet
 if [ $? -ne 0 ]; then rc=1; fi
 echo "mode: count" > profile.cov
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d); do
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d | grep -v vendor); do
 	if ls $dir/*.go &> /dev/null; then
 		go test -covermode=count -coverprofile=$dir/profile.tmp $dir
 		if [ $? -ne 0 ]; then rc=1; fi
