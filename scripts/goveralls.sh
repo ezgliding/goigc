2a71951bdc976101f91cd6cf1147bc978df9fae7
Ricardo Rocha <rocha.porto@gmail.com>
2017-08-08 22:24:27 +0200

    Simplify build scripts, drop useless rc checks

--- scripts/goveralls.sh
+++ scripts/goveralls.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-rc=0
-export PATH=$PATH:$HOME/gopath/bin
 golint .
-if [ $? -ne 0 ]; then rc=1; fi
-go list ./... | grep -v vendor | xargs go vet
-if [ $? -ne 0 ]; then rc=1; fi
+go vet .
 echo "mode: count" > profile.cov
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d | grep -v vendor); do
-	if ls $dir/*.go &> /dev/null; then
-		go test -covermode=count -coverprofile=$dir/profile.tmp $dir
-		if [ $? -ne 0 ]; then rc=1; fi
-		if [ -f $dir/profile.tmp ]; then
-			cat $dir/profile.tmp | tail -n +2 >> profile.cov
-			rm $dir/profile.tmp
-		fi
-	fi
-done
+go test -covermode=count -coverprofile=profile.cov .
 goveralls -coverprofile=profile.cov -service=travis-ci -repotoken $COVERALLS_TOKEN
-if [ $? -ne 0 ]; then rc=1; fi
-exit $rc
