ab4e26f415dd502a6791092bfb69d5b4edc3c6da
Ricardo Rocha <rocha.porto@gmail.com>
2017-08-08 22:24:27 +0200

    Simplify build scripts, drop useless rc checks

--- scripts/pprof.sh
+++ scripts/pprof.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-rc=0
-for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d | grep -v vendor); do
-	if ls ${dir}/*.go &> /dev/null; then
-		cd $dir
-		go test -bench . -cpuprofile=cpu.pprof -memprofile=mem.pprof -run=Benchmark*
-		if [ $? -ne 0 ]; then rc=1; fi
-		if [ -f ${dir}.test ] && [ -f cpu.pprof ]; then
-			go tool pprof -text ${dir}.test cpu.pprof
-			if [ $? -ne 0 ]; then rc=1; fi
-		fi
-		if [ -f ${dir}.test ] && [ -f mem.pprof ]; then
-			go tool pprof -text ${dir}.test mem.pprof
-			if [ $? -ne 0 ]; then rc=1; fi
-		fi
-		rm -f ${dir}.test cpu.pprof mem.pprof
-		cd -
-	fi
-done
-exit $rc
+go test -bench . -cpuprofile=cpu.pprof -memprofile=mem.pprof -run=Benchmark*
+go tool pprof -text cpu.pprof
+go tool pprof -text mem.pprof
