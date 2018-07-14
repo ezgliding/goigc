d5abbb1c8fc461e43b0c32639cae7c9ead10540c
Ricardo Rocha <rocha.porto@gmail.com>
2018-07-14 18:51:41 +0200

    Move all parse tests to files
    Move test to testdata (golang pattern).
    
    Move all tests in parse_test to testdata files, with a pattern:
    testdata/<testname>.[0|1].igc
    
    Golden data goes into:
    testdata/<testname>.[0|1].igc.golden

--- parse_test.go
+++ parse_test.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -32,213 +33,58 @@ type ParseTest struct {
 	e bool
 }
 
-var parseTests = []ParseTest{
-	{
-		"parse-basic-header-test",
-		`
-AFLA001Some Additional Data
-HFDTE010203
-HFFXA500
-HFPLTPilotincharge:EZ PILOT
-HFCM2Crew2:EZ CREW
-HFGTYGliderType:EZ TYPE
-HFGIDGliderID:EZ ID
-HFDTM100GPSDatum:WGS84
-HFRFWFirmwareVersion:v 0.1
-HFRHWHardwareVersion:v 0.2
-HFFTYFRType:EZ RECORDER,001
-HFGPSEZ GPS,002,12,5000
-HFPRSPressAltSensor:EZ PRESSURE
-HFCIDCompetitionID:EZ COMPID
-HFCCLCompetitionClass:EZ COMPCLASS
-HFTZNTimezone:2.00
-`,
-		false,
-	},
-	{"parse-A-record-failure-too-short",
-		"AFLA0", true},
-	{"parse-H-record-failure-too-short",
-		"HFFX", true},
-	{"parse-H-record-failure-bad-date",
-		"HFDTE330203", true},
-	{"parse-H-record-failure-date-too-short",
-		"HFDTE33", true},
-	{"parse-H-record-failure-bad-fix-accuracy",
-		"HFFXAAAA", true},
-	{"parse-H-record-failure-fix-accuracy-too-short",
-		"HFFXA20", true},
-	{"parse-H-record-failure-gps-datum-too-short",
-		"HFDTM20", true},
-	{"parse-H-record-failure-unknown-field",
-		"HFZZZaaa", true},
-	{"parse-H-record-failure-bad-timezone",
-		"HFTZNaa", true},
-	{
-		"parse-basic-flight-test",
-		`
-I033638FXA3940SIU4143ENL
-J010812HDT
-C150701213841160701000102500KTri
-C5111359N00101899WEZ TAKEOFF
-C5110179N00102644WEZ START
-C5209092N00255227WEZ TP1
-C5230147N00017612WEZ TP2
-C5110179N00102644WEZ FINISH
-C5111359N00101899WEZ LANDING
-F160240040609123624
-D20331
-E160245ATS102312
-B1602455107126N00149300WA002880042919509020
-K16024800090
-B1603105107212N00149174WV002930043519608024
-LPLTLOG TEXT
-GREJNGJERJKNJKRE31895478537H43982FJN9248F942389T433T
-GJNJK2489IERGNV3089IVJE9GO398535J3894N358954983O0934
-`,
-		false,
-	},
-	{"parse-point-fix-wrong-size",
-		"B110001", true},
-	{"parse-point-fix-bad-time",
-		"B3103105107212N00149174WV002930043519608024", true},
-	{"parse-point-fix-bad-fix-validity",
-		"B1603105107212N00149174WX002930043519608024", true},
-	{"parse-point-fix-bad-pressure-altitude",
-		"B1603105107212N00149174WV0029a0043519608024", true},
-	{"parse-point-fix-bad-gnss-altitude",
-		"B1603105107212N00149174WV002930043a19608024", true},
-	{"parse-irecord-wrong-size",
-		"I0", true},
-	{"parse-irecord-invalid-value-for-field-number",
-		"I0a", true},
-	{"parse-irecord-wrong-size-with-fields",
-		"I02AAA0102BBB030", true},
-	{"parse-jrecord-wrong-size",
-		"J0", true},
-	{"parse-jrecord-invalid-value-for-field-number",
-		"J0a", true},
-	{"parse-jrecord-wrong-size-with-fields",
-		"J02AAA0102BBB030", true},
-	{"parse-k-wrong-size",
-		"K16024", true},
-	{"parse-k-invalid-date",
-		"K160271", true},
-	{"parse-k-wrong-size",
-		"K16027000090", true},
-	{"parse-e-wrong-size",
-		"E16024", true},
-	{"parse-e-invalid-date",
-		"E160271ATS", true},
-	{"parse-f-wrong-size",
-		"F16024", true},
-	{"parse-f-invalid-date",
-		"F1602710102", true},
-	{"parse-f-invalid-num-satellites",
-		"F1602310a02", true},
-	{"parse-l-wrong-size",
-		"LPL", true},
-	{"parse-c-bad-num-lines",
-		"C150701213841160701000102500KTri", true},
-	{"parse-c-wrong-size-first-line",
-		"C15070121384116070100010", true},
-	{"parse-c-invalid-num-of-tps",
-		"C15070121384116070100010a", true},
-	{"parse-c-invalid-declaration-date",
-		"C350701213841160701000102500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5230147N00017612WEZ TP2\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", false},
-	{"parse-c-invalid-flight-date",
-		"C150701213841360701000102500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5230147N00017612WEZ TP2\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", false},
-	{"parse-c-invalid-task-number",
-		"C150701213841160701000a01500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", true},
-	{"parse-c-invalid-takeoff",
-		"C150701213841160701000101500KTri\nC5111359N00101899\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", true},
-	{"parse-c-invalid-start",
-		"C150701213841160701000101500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644\nC5209092N00255227WEZ TP1\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", true},
-	{"parse-c-invalid-tp",
-		"C150701213841160701000101500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227\nC5110179N00102644WEZ FINISH\nC5111359N00101899WEZ LANDING", true},
-	{"parse-c-invalid-finish",
-		"C150701213841160701000101500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5110179N00102644\nC5111359N00101899WEZ LANDING", true},
-	{"parse-c-invalid-landing",
-		"C150701213841160701000101500KTri\nC5111359N00101899WEZ TAKEOFF\nC5110179N00102644WEZ START\nC5209092N00255227WEZ TP1\nC5110179N00102644WEZ FINISH\nC5111359N00101899", true},
-	{"parse-d-wrong-size",
-		"D2033", true},
-	{"parse-invalid-record",
-		"RANDOM GARBAGE", true},
-}
-
 var update = flag.Bool("update", false, "update golden test data")
 
-func Get(t *testing.T, actual Track, test string) Track {
-	golden := filepath.Join("test", fmt.Sprintf("%s.json", test))
+func runTest(t *testing.T, ok bool, in, out string) {
 
-	actualJSON, err := json.MarshalIndent(actual, "", "  ")
+	data, err := ioutil.ReadFile(in)
 	if err != nil {
-		t.Fatalf("%v :: %+v", err, actual)
+		t.Error(err)
+		return
+	}
+	result, err := Parse(string(data))
+	if err != nil && !ok {
+		return
+	} else if err != nil {
+		t.Error(err)
+	}
+	resultJson, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		t.Fatalf("%v :: %+v", err, result)
 	}
 
+	// update golden if flag is passed
 	if *update {
-		if err = ioutil.WriteFile(golden, actualJSON, 0644); err != nil {
+		if err = ioutil.WriteFile(out, resultJson, 0644); err != nil {
 			t.Fatal(err)
 		}
 	}
 
-	expectedJSON, err := ioutil.ReadFile(golden)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var expected Track
-	err = json.Unmarshal(expectedJSON, &expected)
+	expectedJson, err := ioutil.ReadFile(out)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	return expected
+	if string(resultJson) != string(expectedJson) {
+		t.Errorf("expected\n%+v\ngot\n%+v", string(expectedJson), string(resultJson))
+	}
 }
 
 func TestParse(t *testing.T) {
-	var expected Track
-	for _, test := range parseTests {
-		t.Run(test.t, func(t *testing.T) {
-			result, err := Parse(test.c)
-			if err != nil && test.e {
-				return
-			} else if err != nil {
-				t.Error(err)
-			}
-			expected = Get(t, result, test.t)
-			resultJSON, _ := json.Marshal(result)
-			expectedJSON, _ := json.Marshal(expected)
-			if string(resultJSON) != string(expectedJSON) {
-				t.Errorf("expected\n%+v\ngot\n%+v", string(expectedJSON), string(resultJSON))
-			}
-		})
-	}
-}
-
-func TestParseLocation(t *testing.T) {
-	var expected Track
-	files, err := ioutil.ReadDir("test")
+	// testdata file name format is testname.[1|0].igc
+	match, err := filepath.Glob("testdata/parse-*.igc")
 	if err != nil {
 		t.Fatal(err)
 	}
-	for _, f := range files {
-		match, _ := filepath.Match("parse-location-*.igc", f.Name())
-		if match {
-			t.Run(f.Name(), func(t *testing.T) {
-				result, err := ParseLocation(filepath.Join("test", f.Name()))
-				if err != nil {
-					t.Fatalf("%v :: %v", f.Name(), err)
-				}
+	for _, in := range match {
+		t.Run(in, func(t *testing.T) {
+			parts := strings.Split(in, ".")
+			ok, _ := strconv.ParseBool(parts[len(parts)-2])
+			out := fmt.Sprintf("%v.golden", in)
 
-				expected = Get(t, result, strings.Split(f.Name(), ".igc")[0])
-				resultJSON, _ := json.Marshal(result)
-				expectedJSON, _ := json.Marshal(expected)
-				if string(resultJSON) != string(expectedJSON) {
-					t.Fatalf("%v\nexpected\n%v\nresult\n%v", f.Name(), string(expectedJSON), string(resultJSON))
-				}
-			})
-		}
+			runTest(t, ok, in, out)
+		})
 	}
-
 }
 
 func TestParseLocationMissing(t *testing.T) {
